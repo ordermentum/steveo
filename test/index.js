6ec5e02695ec05ed5e6cbb43e389f97f4173e7b0
Anoop Varghese <anupvarghese@gmail.com>
2017-03-23 10:40:09 +1100

    add tests

---
+++ test/index.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import Steveo from '../lib';
+
+describe('Index', () => {
+  it('should check steveo import', () => {
+    expect(typeof Steveo).to.equal('function');
+  });
+
+  it('should create task', () => {
+    const steveo = Steveo('dev', '127.0.0.1');
+    expect(typeof steveo).to.equal('object');
+    expect(typeof steveo.task).to.equal('object');
+  });
+});
