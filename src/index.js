dd78d6d3490d0950d8c2fb67832a6e5a0375692f
Anoop Varghese <anupvarghese@gmail.com>
2017-03-22 16:57:16 +1100

    index

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// @flow
+import Task from './task';
+import Registry from './registry';
+import Runner from './runner';
+
+const Steveo = (env: string, kafkaHost: string) => {
+  const registeredTopics = {};
+  const registry = Registry(registeredTopics);
+  const runner = Runner(env, kafkaHost, registry);
+  return {
+    task: Task(registry, runner),
+  };
+};
+
+export default Steveo;
