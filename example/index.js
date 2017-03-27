f8616f28bf71aaaf29280c35325d7eae99345166
Anoop Varghese <anupvarghese@gmail.com>
2017-03-27 16:25:14 +1100

    use correct configs

--- example/index.js
+++ example/index.js
@@ -1,19 +1,38 @@
 const Steveo = require('steveo').default;
 const { kafkaCompression } = require('steveo');
 
-const env = Object.assign({}, process.env, { KAFKA_CODEC: kafkaCompression.GZIP });
-
-const steveo = Steveo(env, console);
-
-// wait for the kafka consumer to call the subscribe action
-const subscribe = async (payload) => {
-  console.log('Here is the payload', payload);
+const config = {
+  kafkaConnection: process.env.KAFKA_CONNECTION,
+  kafkaCodec: kafkaCompression.GZIP,
+  clientId: process.env.CLIENT_ID,
+  logLevel: process.env.LOG_LEVEL,
+  kafkaGroupId: '1234',
+  kafkaSendAttempts: process.env.KAFKA_SEND_ATTEMPTS,
+  kafkaSendDelayMin: process.env.KAFKA_SEND_DELAY_MIN,
+  kafkaSendDelayMax: process.env.KAFKA_SEND_DELAY_MAX,
 };
 
-// define task
-steveo.task.define('test-topic', subscribe);
+(async () => {
+  const steveo = Steveo(config, console);
 
-// publish task
-steveo.task.publish({ here: 'is a payload' });
+  // wait for the kafka consumer to call the subscribe action
+  const subscribe = async (payload) => {
+    console.log('Here is the payload', payload);
+  };
 
-process.exit();
+  // define task
+  await steveo.task.define('test-topic', subscribe);
+
+  // publish task
+  await steveo.task.publish({ here: 'is a payload' });
+
+  // get lag
+  const lag = await steveo.fetchConsumerLag('1234', 'test-topic', [0]);
+
+  console.log('********lag', lag);
+  process.exit();
+})().catch((ex) => {
+  console.log('Exception', ex);
+  process.exit();
+});
+
