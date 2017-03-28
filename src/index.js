99b6a209431946e13b59739ce7696abc62a5b276
Anoop Varghese <anupvarghese@gmail.com>
2017-03-28 16:33:26 +1100

    rename to lag, add runner to task

--- src/index.js
+++ src/index.js
@@ -6,16 +6,21 @@ import NULL_LOGGER from 'null-logger';
 import Task from './task';
 import Registry from './registry';
 import Runner from './runner';
+import Admin from './admin';
 
 import type { Config } from '../types';
 
 const Steveo = (config: Config, logger: Object = NULL_LOGGER) => {
   const registeredTopics = {};
-  const registry = Registry(registeredTopics);
-  const runner = Runner(config, registry, logger);
+  const registry = Registry(registeredTopics, config.publishCallback);
+  const task = () => {
+    const runner = Runner(config, registeredTopics, logger);
+    return Task(registry, runner, logger);
+  };
+
   return {
-    task: Task(registry, runner, logger),
-    fetchConsumerLag: runner.fetchConsumerLag,
+    task,
+    lag: Admin(config).lag,
   };
 };
 
