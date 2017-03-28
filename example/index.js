5fc41d4b89132dca1f41a03e0b65a14ae1c6a5cf
Anoop Varghese <anupvarghese@gmail.com>
2017-03-28 16:32:39 +1100

    modified example app

--- example/index.js
+++ example/index.js
@@ -1,6 +1,11 @@
 const Steveo = require('steveo').default;
 const { kafkaCompression } = require('steveo');
 
+const publishCallback = {
+  success: (topic, payload) => console.log(topic, payload, 'Yaaay success'),
+  failure: (topic, payload) => console.log(topic, payload, 'Ohhh failed'),
+};
+
 const config = {
   kafkaConnection: process.env.KAFKA_CONNECTION,
   kafkaCodec: kafkaCompression.GZIP,
@@ -10,6 +15,7 @@ const config = {
   kafkaSendAttempts: process.env.KAFKA_SEND_ATTEMPTS,
   kafkaSendDelayMin: process.env.KAFKA_SEND_DELAY_MIN,
   kafkaSendDelayMax: process.env.KAFKA_SEND_DELAY_MAX,
+  publishCallback,
 };
 
 (async () => {
@@ -17,22 +23,41 @@ const config = {
 
   // wait for the kafka consumer to call the subscribe action
   const subscribe = async (payload) => {
-    console.log('Here is the payload', payload);
+    console.log('First producer payload', payload);
   };
 
+  // task
+  const task = steveo.task();
   // define task
-  await steveo.task.define('test-topic', subscribe);
+  await task.define('test-topic', subscribe);
 
   // publish task
-  await steveo.task.publish({ here: 'is a payload' });
+  await task.publish({ here: 'is first payload' });
 
   // get lag
-  const lag = await steveo.fetchConsumerLag('1234', 'test-topic', [0]);
+  let lag;
+  setTimeout(async () => {
+    lag = await steveo.lag('1234', 'test-topic', [0]);
+    console.log('*******LAG*******', lag);
+  }, 2000);
 
-  console.log('********lag', lag);
-  process.exit();
+  const subscribe2 = async (payload) => {
+    console.log('Second producer payload', payload);
+  };
+
+  // task
+  const task2 = steveo.task();
+  // define task
+  await task2.define('another-test-topic', subscribe2);
+
+  // publish task
+  await task2.publish({ here: 'is second payload' });
+
+  setTimeout(async () => {
+    lag = await steveo.lag('1234', 'another-test-topic', [0]);
+    console.log('*******LAG*******', lag);
+  }, 2000);
 })().catch((ex) => {
   console.log('Exception', ex);
   process.exit();
 });
-
