7b90d7d9493359559a59882a7e14cf9ef3184ac7
Anoop Varghese <anupvarghese@gmail.com>
2017-03-28 16:34:57 +1100

    clean up helper

--- src/helpers/kafka.js
+++ src/helpers/kafka.js
@@ -1,26 +1,26 @@
 // @flow
 import Kafka from 'no-kafka';
-import uuid from 'uuid';
 
 import type { KafkaParams } from '../../types';
 
 const KafkaClient = (params: KafkaParams) => {
-  const producer = new Kafka.Producer({
+  const admin = new Kafka.GroupAdmin({
+    groupId: params.kafkaGroupId,
+    clientId: params.clientId,
     connectionString: params.kafkaConnection,
     codec: params.kafkaCodec,
   });
 
+
   const consumer = new Kafka.GroupConsumer({
     groupId: params.kafkaGroupId,
-    clientId: params.clientId || uuid.v4(),
+    clientId: params.clientId,
     connectionString: params.kafkaConnection,
     codec: params.kafkaCodec,
     logger: params.logger,
   });
 
-  const admin = new Kafka.GroupAdmin({
-    groupId: params.kafkaGroupId,
-    clientId: params.clientId || uuid.v4(),
+  const producer = new Kafka.Producer({
     connectionString: params.kafkaConnection,
     codec: params.kafkaCodec,
   });
