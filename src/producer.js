cb5fb5eb5910a5bc125c1843234adcf44af28bc8
Anoop Varghese <anupvarghese@gmail.com>
2017-05-24 08:07:46 +1000

    log payload size

--- src/producer.js
+++ src/producer.js
@@ -12,11 +12,14 @@ const Producer = (config: Config, registry: Reg, logger: Object) => {
 
   const producerPayload = (msg: Object, topic: string) => {
     const timestamp = moment().unix();
-
+    const payload = JSON.stringify(Object.assign({}, msg, { timestamp }));
+    logger.info('Payload Size:', topic, payload.length);
     return {
       timestamp,
       topic,
-      message: { value: JSON.stringify(Object.assign({}, msg, { timestamp })) },
+      message: {
+        value: payload,
+      },
     };
   };
 
