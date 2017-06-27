3fcf361876faaf8cc4c818a6479113eaf3194420
Anoop Varghese <anupvarghese@gmail.com>
2017-06-27 10:13:59 +1000

    remove elasticmq

--- src/config/sqs.js
+++ src/config/sqs.js
@@ -7,9 +7,6 @@ const sqs = (config) => {
     accessKeyId: config.accessKeyId,
     secretAccessKey: config.secretAccessKey,
   });
-  if (process.env.NODE_ENV === 'development') {
-    instance.setEndpoint(process.env.DEV_SQS);
-  }
   return instance;
 };
 
