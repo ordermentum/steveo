d81b8c33de270b867d6dcfb91e320ca431f31183
Ish <arshusingh16@gmail.com>
2021-09-16 11:15:17 +1000

    fix test

--- test/producer/kafka_test.ts
+++ test/producer/kafka_test.ts
@@ -4,7 +4,7 @@ import Producer from '../../src/producer/kafka';
 import Registry from '../../src/registry';
 
 describe('Kafka Producer', () => {
-  let sandbox;
+  let sandbox: sinon.SinonSandbox;
   beforeEach(() => {
     sandbox = sinon.createSandbox();
   });
@@ -34,7 +34,7 @@ describe('Kafka Producer', () => {
       bootstrapServers: "kafka:9200",
       securityProtocol: 'plaintext'
     }, registry);
-    const sendStub = sandbox.stub(p.producer, 'produce').resolves();
+    const sendStub = sandbox.stub(p.producer, 'produce').callsArgWith(5);
     await p.send('test-topic', { a: 'payload' });
     expect(sendStub.callCount).to.equal(1);
   });
@@ -47,7 +47,7 @@ describe('Kafka Producer', () => {
       bootstrapServers: "kafka:9200",
       securityProtocol: 'plaintext'
     }, registry);
-    const sendStub = sandbox.stub(p.producer, 'produce').throws();
+    const sendStub = sandbox.stub(p.producer, 'produce').callsArgWith(5, 'error');
     let err;
     try {
       await p.send('test-topic', { a: 'payload' });
@@ -66,7 +66,7 @@ describe('Kafka Producer', () => {
       securityProtocol: 'plaintext'
     }, registry);
     registry.addTopic('test-topic');
-    const sendStub = sandbox.stub(p.producer, 'produce').resolves();
+    const sendStub = sandbox.stub(p.producer, 'produce').callsArgWith(5);
     await p.send('test-topic', { a: 'payload', b: '¼' });
     expect(sendStub.callCount).to.equal(1);
     const payload = JSON.parse(sendStub.args[0][2].toString('utf8'));
@@ -81,7 +81,7 @@ describe('Kafka Producer', () => {
       securityProtocol: 'plaintext'
     }, registry);
     registry.addTopic('test-topic');
-    const sendStub = sandbox.stub(p.producer, 'produce').resolves();
+    const sendStub = sandbox.stub(p.producer, 'produce').callsArgWith(5);
     await p.send('test-topic', JSON.stringify({ a: 'payload', b: '¼' }));
     expect(sendStub.args[0][2] instanceof Buffer).to.be.true;
   });
