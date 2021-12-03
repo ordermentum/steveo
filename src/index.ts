270638f4007110ead1e843c1d5eb9615eb40fe71
John D'Agostino <john@ordermentum.com>
2021-12-04 07:28:17 +1100

    add back 14.x for child process spawn

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 import NULL_LOGGER from 'null-logger';
 import { ChildProcess } from 'child_process';
 import { forkChild } from './util';
@@ -43,6 +45,12 @@ export class Steveo implements ISteveo {
 
   childProcesses: Map<number, ChildProcess>;
 
+  restarts: number;
+
+  exiting: boolean;
+
+  MAX_RESTARTS = 20;
+
   constructor(
     configuration: Configuration,
     logger: Logger = NULL_LOGGER,
@@ -55,6 +63,8 @@ export class Steveo implements ISteveo {
     this.events = this.registry.events;
     this.hooks = hooks;
     this.childProcesses = new Map();
+    this.restarts = 0;
+    this.exiting = false;
   }
 
   task<T = any, R = any>(
@@ -112,28 +122,28 @@ export class Steveo implements ISteveo {
 
   private async exitHandler(
     code: number | null,
-    child: ChildProcess,
+    child: ChildProcess | null,
     topic: string
   ) {
-    const { pid } = child;
-    this.childProcesses.delete(pid);
+    const pid = child?.pid;
+
+    if (pid) this.childProcesses.delete(pid);
     if (code === 0 || code === null) {
       this.logger.info(`Child ${pid} terminated`);
       if (this.childProcesses.size === 0) {
         process.exit(1);
       }
-    } else {
-      this.logger.info(`Restarting child ${pid}`);
-      const restarted = await forkChild(
-        topic,
-        this.config.tasksPath,
-        this.config.childProcesses
-      );
-      restarted.on('exit', c =>
-        this.exitHandler.call(this, c, restarted, topic)
-      );
-      this.childProcesses.set(restarted.pid, child);
     }
+
+    if (!this.exiting && this.restarts >= this.MAX_RESTARTS) {
+      this.exiting = true;
+      for (const process of this.childProcesses.values()) {
+        process.kill();
+      }
+    }
+
+    this.logger.info(`Restarting child ${pid}`);
+    await this.startChild(topic);
   }
 
   private async startChild(topic) {
@@ -141,41 +151,60 @@ export class Steveo implements ISteveo {
     return new Promise<void>(async (resolve, reject) => {
       try {
         // Give ample time to let the children start
-        const timeoutID = setTimeout(() => {
+        const timeout = setTimeout(() => {
           reject(new Error('Error forking child'));
         }, 30000);
+
         const child = await forkChild(
           topic,
           this.config.tasksPath,
           this.config.childProcesses
         );
-        child.on('exit', code =>
-          this.exitHandler.call(this, code, child, topic)
-        );
+
+        child.on('exit', code => {
+          clearTimeout(timeout);
+          this.exitHandler.call(this, code, child, topic);
+        });
+        process.on('error', e => {
+          clearTimeout(timeout);
+          this.logger.info(`Failed to start child process ${e}`);
+          this.exitHandler.call(this, null, null, topic);
+        });
         child.on('message', m => {
-          clearTimeout(timeoutID);
+          clearTimeout(timeout);
           if (m === 'success') {
             resolve();
           } else {
             reject();
           }
         });
-        this.childProcesses.set(child.pid, child);
+        if (child.pid) this.childProcesses.set(child.pid, child);
       } catch (e) {
         reject(e);
       }
     });
   }
 
+  /**
+   * This pattern allows us to load all tasks in a directory
+   * and as part of that they self register with the registry allowing
+   */
+  async loadTasks() {
+    require(this.config.tasksPath);
+  }
+
   async start(customTopics: string[] = []) {
-    await import(this.config.tasksPath);
+    await this.loadTasks();
     const runner = this.runner();
+
     const topics = customTopics?.length
       ? customTopics
       : this.registry.getTopics();
+
     const topicsWithRegisteredTasks = topics.filter(
       topic => !!this.registry.getTask(topic)
     );
+
     if (!this.config.childProcesses) {
       runner.process(topicsWithRegisteredTasks);
     } else {
