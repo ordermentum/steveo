021e6eec6d6ebacbb1267a3e15854cc419b380df
Zahav Capper <zahav.capper@ordermentum.com>
2023-01-16 11:37:51 +1100

    add another note

--- src/producer/utils/createMessageMetadata.ts
+++ src/producer/utils/createMessageMetadata.ts
@@ -4,7 +4,7 @@ import os from 'os';
 import newrelic from 'newrelic';
 
 export const createMessageMetadata = <T = any>(message: T, transaction?: newrelic.TransactionHandle) => {
-  const sha1 = crypto.createHash('sha1');
+  const sha1 = crypto.createHash('sha1'); // can we change this to SHA256?
   const signature = sha1
     .update(JSON.stringify(message))
     .digest('hex')
