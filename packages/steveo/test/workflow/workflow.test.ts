b88f9535f89242767fba9be226f761e4e3ad3000
Paul Carroll <paul.carroll@ordermentum.com>
2024-09-05 17:56:49 +1000

    pondering tests

--- packages/steveo/test/workflow/workflow.test.ts
+++ packages/steveo/test/workflow/workflow.test.ts
@@ -1,17 +1,24 @@
-import { flow } from '../../src/workflow/workflow';
+import { Steveo } from '../../src/index';
 import sinon from 'sinon';
 
-describe('', () => {
+// Workflow integration tests
+describe('Workflow tests', () => {
 
   test('should execute flow of two steps', async () => {
+    //
+    const steveo = new Steveo({
+      // TODO: SQS to localstack for tests
+    });
+
     const step1 = sinon.mock();
     const step2 = sinon.mock();
 
-    const definition = flow('test-workflow')
+    steveo
+      .flow('test-workflow', 'test-topic')
       .next({ task: 'step1-task', exec: step1 })
       .next({ task: 'step2-task', exec: step2 })
 
-    await definition.exec('workflow-1');
+
   });
 
   test('', async () => {
