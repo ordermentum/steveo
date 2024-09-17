b9864fe8c5b8d5f469461f6b0bc44a6959a6b14c
Leandro Magalhaes <leandro.magalhaes@ordermentum.com>
2024-09-17 13:42:10 +1000

    Implement dd-trace in DD middleware

--- packages/datadog/src/index.ts
+++ packages/datadog/src/index.ts
@@ -1,9 +1,10 @@
 /* eslint-disable no-underscore-dangle */
-import tracer from 'dd-trace';
+import tracer, { SpanOptions } from 'dd-trace';
 import { Middleware } from 'steveo';
+import { MiddlewareContext } from 'steveo/lib/common';
 
 export class DataDogMiddleware implements Middleware {
-  publish(context, next) {
+  public publish(context, next) {
     return tracer.trace(
       'steveo.publish',
       {
@@ -12,21 +13,42 @@ export class DataDogMiddleware implements Middleware {
           ...context.payload,
         },
       },
-      () => next()
+      (span: tracer.Span | undefined) => {
+        if (span) {
+          const datadogContextData: Record<string, any> = {};
+          tracer.inject(span, 'text_map', datadogContextData);
+          const meta = this.getMetaFromContext(context);
+          meta.datadog = datadogContextData;
+          context.payload._meta = meta;
+        }
+        next(context);
+      }
     );
   }
 
-  async consume(context, next) {
-    return tracer.trace(
-      'steveo.consume',
-      {
-        resource: context.topic,
-        tags: {
-          ...context.payload,
-        },
+  public async consume(context, next) {
+    const tracerOptions: tracer.TraceOptions & SpanOptions = {
+      resource: context.topic,
+      tags: {
+        ...context.payload,
       },
-      () => next()
+    };
+
+    const meta: Record<string, any> = this.getMetaFromContext(context);
+    const datadogContext = meta.datadog ?? {};
+    const spanContext: tracer.SpanContext | null = tracer.extract(
+      'text_map',
+      datadogContext
     );
+    if (spanContext) {
+      tracerOptions.childOf = spanContext;
+    }
+
+    return tracer.trace('steveo.consume', tracerOptions, () => next(context));
+  }
+
+  private getMetaFromContext(context: MiddlewareContext): Record<string, any> {
+    return context.payload._meta ?? {};
   }
 }
 
