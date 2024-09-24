5ea406a6a8de9a8b21372da4a74d8c72e2d66bca
Paul Carroll <paul.carroll@ordermentum.com>
2024-09-24 15:15:20 +1000

    change workflowid to store as uuid

--- packages/storage-postgres/prisma/migrations/20240924051354_init/migration.sql
+++ packages/storage-postgres/prisma/migrations/20240924051354_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "WorkflowState" (
-    "workflowId" TEXT NOT NULL,
+    "workflowId" UUID NOT NULL,
     "serviceId" TEXT NOT NULL,
     "started" TIMESTAMPTZ(6) NOT NULL,
     "completed" TIMESTAMPTZ(6),
