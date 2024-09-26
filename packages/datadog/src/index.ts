f0a51dab73232ace162d3549fdfb023dda40c621
Leandro Magalhães <leandro.magalhaes@ordermentum.com>
2024-09-26 11:01:56 +1000

    [PLATFORMS-1544] Implement dd-trace in DD middleware (#804)
    * Implement dd-trace in DD middleware and unit tests
    * Include test folder in tsconfig file
    * Add changeset

--- packages/datadog/src/index.ts
+++ packages/datadog/src/index.ts
@@ -1,9 +1,12 @@
 /* eslint-disable no-underscore-dangle */
-import tracer from 'dd-trace';
+import tracer, { SpanOptions } from 'dd-trace';
 import { Middleware } from 'steveo';
+import { MiddlewareContext } from 'steveo/lib/common';
 
 export class DataDogMiddleware implements Middleware {
-  publish(context, next) {
+  public readonly DATADOG_CONTEXT_FORMAT: string = 'text_map';
+
+  public publish(context, next) {
     return tracer.trace(
       'steveo.publish',
       {
@@ -12,21 +15,49 @@ export class DataDogMiddleware implements Middleware {
           ...context.payload,
         },
       },
-      () => next()
+      (span: tracer.Span | undefined) => {
+        if (span) {
+          const messageMetadata = this.getMetaFromContext(context);
+          const datadogContextData: Record<string, any> =
+            this.getDatadogContextData(span);
+          messageMetadata.datadog = datadogContextData;
+          context.payload._meta = messageMetadata;
+        }
+        next(context);
+      }
     );
   }
 
-  async consume(context, next) {
-    return tracer.trace(
-      'steveo.consume',
-      {
-        resource: context.topic,
-        tags: {
-          ...context.payload,
-        },
+  public async consume(context, next) {
+    const tracerOptions: tracer.TraceOptions & SpanOptions = {
+      resource: context.topic,
+      tags: {
+        ...context.payload,
       },
-      () => next()
+    };
+
+    const messageMetadata: Record<string, any> =
+      this.getMetaFromContext(context);
+    const datadogContext: Record<string, any> = messageMetadata.datadog ?? {};
+    const spanContext: tracer.SpanContext | null = tracer.extract(
+      this.DATADOG_CONTEXT_FORMAT,
+      datadogContext
     );
+    if (spanContext) {
+      tracerOptions.childOf = spanContext;
+    }
+
+    return tracer.trace('steveo.consume', tracerOptions, () => next(context));
+  }
+
+  private getMetaFromContext(context: MiddlewareContext): Record<string, any> {
+    return context.payload._meta ?? {};
+  }
+
+  private getDatadogContextData(datadogSpan: tracer.Span): Record<string, any> {
+    const contextData: Record<string, any> = {};
+    tracer.inject(datadogSpan, this.DATADOG_CONTEXT_FORMAT, contextData);
+    return contextData;
   }
 }
 
