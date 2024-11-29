33cde070700d5665717f93fec778134c3e588d0a
Zahav Capper <zahav.capper@ordermentum.com>
2024-11-29 13:06:53 +0800

    dedupe code and add error logging

--- packages/cloudwatch/src/index.ts
+++ packages/cloudwatch/src/index.ts
@@ -1,6 +1,7 @@
 import {
   CloudWatchClient,
   PutMetricDataCommand,
+  PutMetricDataCommandOutput,
 } from '@aws-sdk/client-cloudwatch';
 import {
   Job as PrismaJob,
@@ -18,127 +19,97 @@ import {
 type Job = PrismaJob | JobInstance;
 type JobAttributes = SequelizeJob | PrismaJob;
 
+const Namespace = 'Steveo-DB-Jobs';
 const client = new CloudWatchClient({
   region:
     process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION || 'us-east-1',
 });
-const Namespace = 'Steveo-DB-Jobs';
+const publishCountForService = (
+  metricName: string,
+  serviceTag: string,
+  statusTag: string,
+  count: number
+): Promise<PutMetricDataCommandOutput> => {
+  const command = new PutMetricDataCommand({
+    MetricData: [
+      {
+        MetricName: metricName,
+        Dimensions: [
+          {
+            Name: 'service',
+            Value: serviceTag,
+          },
+          {
+            Name: 'status',
+            Value: statusTag,
+          },
+        ],
+        Unit: 'Count',
+        Timestamp: new Date(),
+        Value: count,
+      },
+    ],
+    Namespace,
+  });
+  return client.send(command);
+};
 
 export const schedulerMetrics = (
   scheduler: PrismaScheduler | SequelizeScheduler,
   service: string
 ) => {
   scheduler.events.on('duration', (job: Job) => {
-    const MetricName = job.name.toUpperCase();
-    const command = new PutMetricDataCommand({
-      MetricData: [
-        {
-          MetricName,
-          Dimensions: [
-            {
-              Name: 'service',
-              Value: service,
-            },
-            {
-              Name: 'status',
-              Value: 'completed',
-            },
-          ],
-          Unit: 'Count',
-          Timestamp: new Date(),
-          Value: 1,
-        },
-      ],
-      Namespace,
-    });
-
-    client.send(command);
+    try {
+      const jobName = job.name.toUpperCase();
+      publishCountForService(jobName, service, 'completed', 1);
+    } catch (err) {
+      scheduler.logger.error(
+        `steveo-cloudwatch: Error while putting metrics: ${err}`
+      );
+      throw err;
+    }
   });
 
   scheduler.events.on('lagged', (jobs: JobAttributes[]) => {
     for (const job of jobs) {
-      const MetricName = job.name.toUpperCase();
-      const command = new PutMetricDataCommand({
-        MetricData: [
-          {
-            MetricName,
-            Dimensions: [
-              {
-                Name: 'service',
-                Value: service,
-              },
-              {
-                Name: 'status',
-                Value: 'stuck',
-              },
-            ],
-            Unit: 'Count',
-            Timestamp: new Date(),
-            Value: 1,
-          },
-        ],
-        Namespace,
-      });
-
-      client.send(command, err => {
-        if (err) throw err;
-      });
+      try {
+        const jobName = job.name.toUpperCase();
+        publishCountForService(jobName, service, 'stuck', 1);
+      } catch (err) {
+        scheduler.logger.error(
+          `steveo-cloudwatch: Error while putting metrics: ${err}`
+        );
+        throw err;
+      }
     }
   });
 
   scheduler.events.on('reset', (job: JobAttributes) => {
-    const MetricName = job.name.toUpperCase();
-    const command = new PutMetricDataCommand({
-      MetricData: [
-        {
-          MetricName,
-          Dimensions: [
-            {
-              Name: 'service',
-              Value: service,
-            },
-            {
-              Name: 'status',
-              Value: 'restarted',
-            },
-          ],
-          Unit: 'Count',
-          Timestamp: new Date(),
-          Value: 1,
-        },
-      ],
-      Namespace,
-    });
-
-    client.send(command);
+    try {
+      const jobName = job.name.toUpperCase();
+      publishCountForService(jobName, service, 'restarted', 1);
+    } catch (err) {
+      scheduler.logger.error(
+        `steveo-cloudwatch: Error while putting metrics: ${err}`
+      );
+      throw err;
+    }
   });
 
   scheduler.events.on('pending', (data: PendingJobs) => {
     for (const [name, count] of Object.entries(data)) {
-      const MetricName = name.toUpperCase();
-      const command = new PutMetricDataCommand({
-        MetricData: [
-          {
-            MetricName,
-            Dimensions: [
-              {
-                Name: 'service',
-                Value: service,
-              },
-              {
-                Name: 'status',
-                Value: 'pending',
-              },
-            ],
-            Unit: 'Count',
-            Timestamp: new Date(),
-            Value: count,
-          },
-        ],
-        Namespace,
-      });
-
-      client.send(command);
+      try {
+        const jobName = name.toUpperCase();
+        if (typeof count !== 'number' || !Number.isFinite(count)) {
+          throw new Error(`Recieved invalid argument for count: ${count}`);
+        }
+        publishCountForService(jobName, service, 'pending', count);
+      } catch (err) {
+        scheduler.logger.error(
+          `steveo-cloudwatch: Error while putting metrics: ${err}`
+        );
+        throw err;
+      }
     }
   });
 };
