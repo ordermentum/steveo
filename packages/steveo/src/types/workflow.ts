c712a5f22bda94b926eaa247780b6d87bf9dff13
Paul Carroll <paul.carroll@gmail.com>
2024-12-04 11:46:10 +1100

    add workflow deletion option on completion (#830)
    * add workflow deletion option on completion
    
    * fix delete workflow test

--- packages/steveo/src/types/workflow.ts
+++ packages/steveo/src/types/workflow.ts
@@ -13,7 +13,16 @@ export interface WorkflowPayload {
  */
 export interface WorkflowOptions extends TaskOptions {
   /**
-   *
+   * Debug ID for this service
+   * Will be used to detect cross service workflow message processing too.
    */
   serviceId: string;
+
+  /**
+   * If true the engine will delete the workflow from storage.
+   * This is used in production systems as workflow states are considered
+   * transient, whereas developers may disable this for development and
+   * diagnostics.
+   */
+  deleteOnComplete: boolean;
 }
